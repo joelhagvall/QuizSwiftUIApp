8d88113c0b30e6aea87e24b05dae0666b0b1c052
joelhagvall <joel.hagvall1@gmail.com>
2025-02-21 21:44:02 +0100

    Initial Commit

---
+++ QuizSwiftUIApp/QuizSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSwiftUIAppApp.swift
+//  QuizSwiftUIApp
+//
+//  Created by Joel Hägvall on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct QuizSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizSwiftUIAppTests/QuizSwiftUIAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuizSwiftUIAppTests.swift
+//  QuizSwiftUIAppTests
+//
+//  Created by Joel Hägvall on 2025-02-21.
+//
+
+import Testing
+
+struct QuizSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
